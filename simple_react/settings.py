9d967eafefc66683e569c320dcdf0d935c582f55
Artem <artem.volshynskyi@gmail.com>
2019-02-23 14:37:12 +0200

    Add default setting, show .env. files

--- simple_react/settings.py
+++ simple_react/settings.py
@@ -79,9 +79,9 @@ WSGI_APPLICATION = 'simple_react.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get("POSTGRES_DB"),
-        'USER': os.environ.get("POSTGRES_USER"),
-        'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
+        'NAME': os.environ.get("POSTGRES_DB", "simple"),
+        'USER': os.environ.get("POSTGRES_USER", "postgres"),
+        'PASSWORD': os.environ.get("POSTGRES_PASSWORD", "password"),
         'HOST': os.environ.get("POSTGRES_HOST", "db"),
         'PORT': os.environ.get("POSTGRES_PORT", "5432")
     }
